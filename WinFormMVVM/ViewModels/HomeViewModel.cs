f9c5bd22704c013797d4755e9359e32bc908a96f
Nelson Rodriguez <n.rodriguez@LumaSenseInc.com>
2014-12-03 16:39:01 -0800

    Original Author info

--- WinFormMVVM/ViewModels/HomeViewModel.cs
+++ WinFormMVVM/ViewModels/HomeViewModel.cs
@@ -4,6 +4,7 @@ using ReactiveUI;
 namespace WinFormMVVM.ViewModels
 {
     /// <summary>
+    ///
     /// ViewModel that will process data and events
     /// The ViewModel inherits from the ReactiveObject class. 
     /// This class  grants your classes access to the RaiseAndSetIfChanged helper function. 
@@ -18,6 +19,9 @@ namespace WinFormMVVM.ViewModels
     /// which will update the status message. Note that the ViewModel has no references to 
     /// any UI elements, which allows us to perform testing without worrying about the 
     /// particular UI technology used.
+    /// 
+    /// (From http://www.codeproject.com/Articles/801986/Using-ReactiveUI-for-WinForms-MVVM-Design#xx4876432xx
+    ///  written by http://www.codeproject.com/Members/User-10914367)
     /// </summary>
     public class HomeViewModel : ReactiveUI.ReactiveObject
     {
